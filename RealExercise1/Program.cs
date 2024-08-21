9f2238efd079c72a063aef37139fb98d361066c3
rsgoodfellow <rsgoodfellow@amarillocollege.com>
2024-08-21 16:51:24 -0500

    Add project files.

---
+++ RealExercise1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RealExercise1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
